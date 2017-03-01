56b81b8c24fb095843a6877d952fb31ed118e3b0
Dawid Mydlo <dmydloinf@gmail.com>
2017-03-01 19:32:03 +0100

    new start

--- pet.py
+++ pet.py
@@ -2,13 +2,11 @@ from subprocess import *
 import sys
 
 
-pet_project_start = "/home/dawid/PycharmProjects/pet/project_start"
-pet_project_tmp_bashrc = "/home/dawid/PycharmProjects/pet/tmp_bashrc"
 pet_project_name = "project"
-pet_project_folder = "/home/dawid/PycharmProjects/pet"
+pet_project_folder = "./project_folder"
 
 if len(sys.argv) > 1:
     print("You choose: ", str(sys.argv[1:]))
 
-run(["./start.sh", pet_project_start, pet_project_name, pet_project_tmp_bashrc, pet_project_folder])
+run(["./start.sh", pet_project_name, pet_project_folder])
 print("EXITED")
--- start.sh
+++ start.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 if [ "$SHELL" = "/bin/bash" ]; then
-    echo "source ~/.bashrc" > $4/tmp_bashrc
-    echo "source ~/.profile" >> $4/tmp_bashrc
-    echo "pet_project_name=$2" >> $4/tmp_bashrc
-    echo "source $1" >> $4/tmp_bashrc
+    echo "source ~/.bashrc" > $2/tmp_bashrc
+    echo "source ~/.profile" >> $2/tmp_bashrc
+    echo "pet_project_name=$1" >> $2/tmp_bashrc
+    echo "source $4/project_start" >> $2/tmp_bashrc
     #bash other
-    #$SHELL --init-file $3
-    $SHELL --rcfile $4/tmp_bashrc
+    #$SHELL --init-file $2
+    $SHELL --rcfile $2/tmp_bashrc
 # TO TEST!
 elif [ "$SHELL" = "/bin/zsh" ]; then
     if [ "$ZDOTDIR" ]; then
-        echo "source $ZDOTDIR/.zshrc" > $4/.zshrc
+        echo "source $ZDOTDIR/.zshrc" > $2/.zshrc
     else
-        echo "source $HOME/.zshrc" > $4/.zshrc
+        echo "source $HOME/.zshrc" > $2/.zshrc
     fi
-    echo "pet_project_name=$2" >> $4/.zshrc
-    echo "source $1" >> $4/.zshrc
+    echo "pet_project_name=$1" >> $2/.zshrc
+    echo "source $2/project_start" >> $2/.zshrc
     #zsh other
-    ZDOTDIR=$4 $SHELL
+    ZDOTDIR=$2 $SHELL
 else
     echo 'havent found correct $SHELL (/bin/bash, /bin/zsh)'
 fi
