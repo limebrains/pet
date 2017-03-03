83495b657d4a64c5f6edb3ce1ae22dbd688028df
Dawid Mydlo <dmydloinf@gmail.com>
2017-03-03 12:38:35 +0100

    pet with basic click after chemotherapy

--- pet.py
+++ pet.py
@@ -1,12 +1,59 @@
+#!/usr/bin/env python3
 from subprocess import *
-import sys
+import os
+import yaml
+import click
 
 
-pet_project_name = "project"
-pet_project_folder = "./project_folder"
+cli = click.Group()
 
-if len(sys.argv) > 1:
-    print("You choose: ", str(sys.argv[1:]))
 
-run(["./start.sh", pet_project_name, pet_project_folder])
-print("EXITED")
+@cli.command()
+@click.argument('name')
+def start(name):
+    """starts new project"""
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f)
+
+    if name in config["project_names"] and name not in config["active"]:
+        start_project(name, config)
+    else:
+        # correlates with nothing - to do here or... probably click will handle command not found
+        pass
+
+
+def start_project(name, config):
+    ppid = os.getppid()
+    config["active"][name] = ppid
+    with open("config.yaml", "w") as f:
+        yaml.dump(config, f)
+    Popen(["./start.sh", name, "./projects/{}".format(name)]).communicate(input)
+    config["active"].pop(name)
+    with open("config.yaml", "w") as f:
+        yaml.dump(config, f)
+
+
+@cli.command()
+def stop():
+    pass
+
+
+@cli.command()
+@click.argument('name')
+@click.option('--temp', '-m', multiple=True, default=None, help='Creates project using templates')
+def create(name, temp):
+    """creates new project"""
+    print(name, temp)
+
+
+@cli.command()
+def list():
+    """lists all projects"""
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f)
+    for name in config["project_names"]:
+        click.echo(name)
+
+
+if __name__ == '__main__':
+    cli()
--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ if [ "$SHELL" = "/bin/bash" ]; then
     echo "source ~/.bashrc" > $2/tmp_bashrc
     echo "source ~/.profile" >> $2/tmp_bashrc
     echo "pet_project_name=$1" >> $2/tmp_bashrc
-    echo "source $4/project_start" >> $2/tmp_bashrc
+    echo "source $2/project_start" >> $2/tmp_bashrc
+    echo "PS1=\"[$1] \$PS1\"" >> $2/tmp_bashrc
     #bash other
     #$SHELL --init-file $2
     $SHELL --rcfile $2/tmp_bashrc
-# TO TEST!
+    $SHELL $2/project_stop
 elif [ "$SHELL" = "/bin/zsh" ]; then
     if [ "$ZDOTDIR" ]; then
         echo "source $ZDOTDIR/.zshrc" > $2/.zshrc
@@ -17,14 +18,10 @@ elif [ "$SHELL" = "/bin/zsh" ]; then
     fi
     echo "pet_project_name=$1" >> $2/.zshrc
     echo "source $2/project_start" >> $2/.zshrc
+    echo "PS1=\"[$1] \$PS1\"" >> $2/.zshrc
     #zsh other
     ZDOTDIR=$2 $SHELL
+    $SHELL $2/project_stop
 else
     echo 'havent found correct $SHELL (/bin/bash, /bin/zsh)'
 fi
-
-
-
-
-
-
