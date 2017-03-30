205dca8cb7d17b8644b9e1f6301774f5dbf1abc8
Dawid Mydlo <dmydloinf@gmail.com>
2017-03-30 17:45:42 +0200

    quick fix

--- pet/utils.py
+++ pet/utils.py
@@ -2,8 +2,9 @@ import os
 
 
 def makedirs(exists_ok=False, *args, **kwargs):
+    path = kwargs.pop('path')
     try:
-        return os.makedirs(*args, **kwargs)
+        return os.makedirs(path, *args, **kwargs)
     except OSError as e:
         if not exists_ok:
             raise e
