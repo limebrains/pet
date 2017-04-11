f7c6029d8517fc6ef0430b7ea74670f28af1db01
Dawid Mydlo <dmydloinf@gmail.com>
2017-04-11 11:27:27 +0200

    install directory

--- install.bash
+++ install.bash
@@ -10,23 +10,34 @@ else
         PET_FOLDER="${PET_FOLDER:0:${#PET_FOLDER}-1}"
     fi
 fi
+if [ -z "$install_dir" ]; then
+    install_dir="."
+else
+    if [ "${install_dir:0:1}" == "~" ]; then
+        install_dir="${HOME}${install_dir:1}"
+    fi
+    if [ "${install_dir: -1}" == "/" ]; then
+        install_dir="${install_dir:0:${#install_dir}-1}"
+    fi
+fi
 
 curl -fsSL https://github.com/dmydlo/pet/archive/master.zip -o pet.zip
 if [ ! -f pet.zip ]; then
     printf "\n\e[1;31mInstallation unsuccessful due to failed download\e[0m\n"
-    return 1
+    exit
 fi
-unzip -o pet.zip
-if [ ! -d "pet-master" ]; then
+unzip -o pet.zip -d "$install_dir"
+rm -f pet.zip
+if [ ! -d "$install_dir/pet-master" ]; then
     printf "\n\e[1;31mInstallation unsuccessful due to failed unzip\e[0m\n"
-    return 1
+    exit
 fi
-pip install -e pet-master/
+pip install -e "$install_dir/pet-master/"
 printf "\n------------------------\n-Installing rest of pet-\n------------------------\n"
 if [ "$USER" == 'root' ]; then
     printf "\n\n\e[1;31mWarning (used as root): During first run use\npet recreate\e[0m\n"
 else
-    python 'pet-master/pet/cli.py' 'recreate'
+    python "$install_dir/pet-master/pet/cli.py" 'recreate'
 fi
 printf "\n------------------------\n-auto-completion deploy-\n------------------------\n"
 printf "\n\e[1;33mAuto-completion requires sudo\e[0m\n"
@@ -37,6 +48,6 @@ if [ "$USER" == 'root' ]; then
         sudo python 'pet-master/pet/cli.py' 'deploy' '-s' "$shell"
     fi
 else
-    printf "\n\e[1;33mNeeds sudo - use 'pet deploy'\e[0m\n"
+    printf "\n\e[1;33mNeeds sudo - use 'sudo pet deploy'\e[0m\n"
 fi
 printf "\n\e[1;32mInstallation completed\e[0m\n"
